544145886a5010720d6820bd9b5ccf860cd869a2
ShrifGamal <154748642+ShrifGamal@users.noreply.github.com>
2026-03-13 04:48:13 +0200

    Add files via upload

---
+++ Company.BAL/Data/Configrations/EmployeeConfigration.cs
@@ -0,0 +1,19 @@
+﻿using Company.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.DAL.Data.Configrations
+{
+    public class EmployeeConfigration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.Property(E => E.Salary).HasColumnType("decimal(18,2)");
+        }
+    }
+}
---
+++ Company.BAL/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.DAL.Models
+{
+    public class Department : BaseEntity
+    {
+        [Required(ErrorMessage = "Code Is Reuired!")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "Name Is Reuired!")]
+        public string Name { get; set; }
+        [DisplayName("Date Of Creation")]
+        public DateTime DateOfCreation { get; set; }
+        public ICollection<Employee>? Employees { get; set; }
+    }
+}
