2882763a5056e9a6fbd96a4e78fab8c1ac68d0ea
ShrifGamal <154748642+ShrifGamal@users.noreply.github.com>
2026-03-13 04:52:15 +0200

    Add files via upload

---
+++ Company.PL/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+let element = document.getElementById("id");
+element.AddEventListener("Keyup", () => {
+    let xhr = new XMLHttpRequest();
+    let url = `https://localhost:44302/Employees/Index?InputSearch=${element.value}`;
+    xhr.open("Post", url, true);
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    }
+    // Sending our request 
+    xhr.send();
+});
